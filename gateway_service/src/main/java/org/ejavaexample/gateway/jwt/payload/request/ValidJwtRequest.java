dbb1529a1de24d141a4a7cabd43f6ac146472c60
ojhgg <75611114+ojhgg@users.noreply.github.com>
2023-06-23 11:18:34 +0800

    网关配置，去掉过滤器

--- gateway_service/src/main/java/org/ejavaexample/gateway/jwt/payload/request/ValidJwtRequest.java
+++ gateway_service/src/main/java/org/ejavaexample/gateway/jwt/payload/request/ValidJwtRequest.java
@@ -1,17 +1,17 @@
-package org.ejavaexample.gateway.jwt.payload.request;
-
-import jakarta.validation.constraints.NotBlank;
-
-public class ValidJwtRequest {
-  @NotBlank
-  private String jwt;
-
-	public String getJwt() {
-		return jwt;
-	}
-
-	public void setJwt(String jwt) {
-		this.jwt = jwt;
-	}
-
-}
+//package org.ejavaexample.gateway.jwt.payload.request;
+//
+//import jakarta.validation.constraints.NotBlank;
+//
+//public class ValidJwtRequest {
+//  @NotBlank
+//  private String jwt;
+//
+//	public String getJwt() {
+//		return jwt;
+//	}
+//
+//	public void setJwt(String jwt) {
+//		this.jwt = jwt;
+//	}
+//
+//}
